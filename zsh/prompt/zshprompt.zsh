5402f52fd666b489c31089d3e8752994de824c24
Lyndsy Simon <lyndsy@lyndsysimon.com>
2017-02-17 12:09:35 -0500

    Remove deprecated prompt config

--- zsh/prompt/zshprompt.zsh
+++
@@ -1,4 +0,0 @@
-RPROMPT='$(zshprompt.py right --last-exit-status=$?)'
-PROMPT='$(zshprompt.py left)'
-# Since we're already displaying venv names, disable virtualenv's prepending.
-export VIRTUAL_ENV_DISABLE_PROMPT=1
