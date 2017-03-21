aa182f2ecfbc770feefcc9a7c10909200527101a
Lyndsy Simon <lyndsy@lyndsysimon.com>
2017-03-21 13:46:06 -0400

    Remove legacy vim config - I'm officially commiting to neovim

--- link.sh
+++ link.sh
@@ -6,14 +6,6 @@ ln -si $dir/zsh/zshrc ~/.zshrc
 # System-accessible custom scripts
 ln -si $dir/bin ~/.bin
 
-# Vim
-ln -si $dir/vim/vimrc ~/.vimrc
-ln -si $dir/vim/vim ~/.vim
-
-# Neovim
-ln -si ~/.vimrc ~/.nvimrc
-ln -si ~/.nvim ~/.vim
-
 # Git
 ln -si $dir/git/gitconfig ~/.gitconfig
 
